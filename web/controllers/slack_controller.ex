71424b7d9d69912b5db01dd6a85df4e342a40db7
Adam Bogdał <adam@bogdal.pl>
2016-08-10 10:31:27 +0200

    Redirect to auth url

--- web/controllers/slack_controller.ex
+++ web/controllers/slack_controller.ex
@@ -1,11 +1,27 @@
 defmodule Foosball.SlackController do
+  require Logger
   use Foosball.Web, :controller
 
   def index(conn, _params) do
-    text conn, "Slack request"
+    Logger.debug "Slack commands/messages"
+    redirect conn, to: "/"
+  end
+
+  def oauth(conn, %{"code" => code}) do
+    Logger.debug "Logging this text!"
+
+    text conn, "Oauth"
+    redirect conn, external: "http://elixir-lang.org/"
   end
 
   def oauth(conn, _params) do
-    text conn, "Oauth"
+    params = [
+      "client_id": System.get_env("SLACK_CLIENT_ID"),
+      "redirect_url": "https://" <> conn.host,
+      "scope": "commands, chat:write:bot"
+    ]
+    auth_url = "https://slack.com/oauth/authorize" <> "?" <> Plug.Conn.Query.encode(params)
+    redirect conn, external: auth_url
   end
+
 end
